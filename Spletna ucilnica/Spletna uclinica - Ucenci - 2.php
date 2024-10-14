bb1dbd602cdf550257e50e2e50a9ab99db98593f
mihazvegler <miha.zvegler1@gmail.com>
2024-10-14 18:56:08 +0200

    Update Spletna uclinica - Ucenci - 2.php

--- Spletna ucilnica/Spletna uclinica - Ucenci - 2.php
+++ Spletna ucilnica/Spletna uclinica - Ucenci - 2.php
@@ -1,73 +1,97 @@
+<?php
+session_start();
+if (!isset($_SESSION['logged_in'])) {
+    // Če uporabnik ni prijavljen, ga preusmerimo na prijavno stran
+    header("Location: login.php");
+    exit();
+}
+
+// Povezava z bazo podatkov
+$link = new mysqli("localhost", "root", "", "SpletnaUcilnica");
+
+if ($link->connect_error) {
+    die("Povezava ni uspela: " . $link->connect_error);
+}
+
+// Pridobimo gradiva za pregled, kjer je prijavljen uporabnik učitelj
+$id_ucitelja = $_SESSION['user_id'];
+$query = "SELECT naslov_gradiva, navodilo, rok_oddaje FROM Gradiva WHERE id_ucitelja = ?";
+$stmt = $link->prepare($query);
+$stmt->bind_param("i", $id_ucitelja);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$gradiva = [];
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $gradiva[] = $row;
+    }
+}
+
+$stmt->close();
+$link->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Spletna učilnica</title>
+    <title>Spletna učilnica - Naloge za pregled</title>
     <link rel='stylesheet' type='text/css' media='screen' href='Spletna ucilnica CSS.css'>
     <script src='main.js'></script>
 </head>
 <body>
 <header>
-        <ul id="headerList">
-            <li><img id="logoPic"src="Slike/book.png"></li>
-            <li id="headerText">SPLETNA UČILNICA</li>
-            <li><img style="height: 80px;margin: 10% 0 0 0;"src="Slike/user.png"></li>
-        </ul>
+    <ul id="headerList">
+        <li><img id="logoPic" src="Slike/book.png"></li>
+        <li id="headerText">SPLETNA UČILNICA</li>
+        <li class="user-info">
+            <!-- Prikaz imena in priimka prijavljenega uporabnika -->
+            <?php echo $_SESSION['user_name']; ?>
+            <!-- Povezava za odjavo -->
+            <a href="logout.php" style="background-color: #f44336; color: white; padding: 5px 10px; border-radius: 5px;">Odjava</a>
+        </li>
+    </ul>
 </header>
 
-    <div class="outerDiv">
-        <div class="navigationDiv">
-            <ul id="navigationList">
-                <li onclick = "OpenNew()">PREDMETI</li>
-                <li style="background-color:grey;">ZA PREGLED</li>
-            </ul>
-        </div>
-        
-        <script>
-            function OpenNew(){
-                window.open("Spletna%20uclinica%20-%20Ucenci.php","_self");
-            }
-            function toggleDetails(element) {
-                var details = element.nextElementSibling;
-                details.classList.toggle("open");
-}
-
-        </script>
-        <div class="assignmentSection">
-        <h2>Seznam nalog</h2>
-        <ul class="assignmentList">
-            <li class="assignmentItem">
-                <div class="assignmentTitle" onclick="toggleDetails(this)">Matematika: Reši enačbe</div>
-                <div class="assignmentDetails">
-                    <p>Opis: Reši 10 enačb različnih težavnosti, ki vključujejo kvadratne enačbe in logaritme.</p>
-                    <p>Rok: 30. september 2024</p>
-                </div>
-            </li>
-            <li class="assignmentItem">
-                <div class="assignmentTitle" onclick="toggleDetails(this)">Slovenščina: Napiši esej</div>
-                <div class="assignmentDetails">
-                    <p>Opis: Napiši esej na temo "Pomembnost slovenskega jezika v sodobni družbi".</p>
-                    <p>Rok: 1. oktober 2024</p>
-                </div>
-            </li>
-            <li class="assignmentItem">
-                <div class="assignmentTitle" onclick="toggleDetails(this)">Zgodovina: Pripravi predstavitev</div>
-                <div class="assignmentDetails">
-                    <p>Opis: Pripravi 10-minutno predstavitev o vzrokih prve svetovne vojne.</p>
-                    <p>Rok: 28. september 2024</p>
-                </div>
-            </li>
-            <li class="assignmentItem">
-                <div class="assignmentTitle" onclick="toggleDetails(this)">Kemija: Eksperimentiraj</div>
-                <div class="assignmentDetails">
-                    <p>Opis: Opravi eksperiment, kjer preizkusiš kislost različnih gospodinjskih snovi.</p>
-                    <p>Rok: 2. oktober 2024</p>
-                </div>
-            </li>
+<div class="outerDiv">
+    <div class="navigationDiv">
+        <ul id="navigationList">
+            <li onclick="window.location.href='Spletna%20uclinica%20-%20Ucenci.php'">PREDMETI</li>
+            <li style="background-color: grey;">ZA PREGLED</li>
         </ul>
     </div>
+
+    <div class="assignmentSection">
+        <h2>Naloge za pregled</h2>
+        <ul class="assignmentList">
+            <?php if (empty($gradiva)): ?>
+                <li class="assignmentItem">Trenutno ni nalog za pregled.</li>
+            <?php else: ?>
+                <?php foreach ($gradiva as $gradiva): ?>
+                    <li class="assignmentItem">
+                        <div class="assignmentTitle" onclick="toggleDetails(this)">
+                            <?php echo htmlspecialchars($gradiva['naslov_gradiva']); ?>
+                        </div>
+                        <div class="assignmentDetails">
+                            <p><?php echo htmlspecialchars($gradiva['navodilo']); ?></p>
+                            <p>Rok oddaje: <?php echo htmlspecialchars($gradiva['rok_oddaje']); ?></p>
+                        </div>
+                    </li>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </ul>
     </div>
+</div>
+
+<script>
+    function toggleDetails(element) {
+        var details = element.nextElementSibling;
+        details.classList.toggle("open");
+    }
+</script>
+
 </body>
 </html>
